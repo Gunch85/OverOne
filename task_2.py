b9b2c5fe20735c41da715e2142d51690c81adaa9
Gunch85 <gunch1985@gmail.com>
2022-06-06 20:03:00 +0300

    triangle

---
+++ task_2.py
@@ -0,0 +1,13 @@
+print("Стороны:")
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+
+if a + b <= c or a + c <= b or b + c <= a:
+    print("Треугольник не существует")
+elif a != b and a != c and b != c:
+    print("Разносторонний")
+elif a == b == c:
+    print("Равносторонний")
+else:
+    print("Равнобедренный")
